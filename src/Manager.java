e98055d491dc2ce6c1150414ba00d4f74e149142
unknown <andreyjk13.gmail.com>
2024-04-05 10:41:04 +0300

    second commit

--- src/Manager.java
+++ src/Manager.java
@@ -9,4 +9,15 @@ public class Manager {
            forks[i] = new Semaphore(1);
        }
    }
+   public int MaxEating(int[] arr){
+       int max = arr[0];
+       int id = 0;
+       for(int i = 0; i < arr.length; i ++) {
+           if (max <= arr[i]) {
+               max = arr[i];
+               id = i;
+           }
+       }
+       return id;
+   }
 }
--- src/Phylosophers.java
+++ src/Phylosophers.java
@@ -3,6 +3,7 @@ import java.util.concurrent.Delayed;
 public class Phylosophers implements Runnable {
     private final int id;
     private final Manager manager;
+    public int[] arr = {0,0,0,0,0};
     public Phylosophers(int id, Manager manager) {
         this.id = id;
         this.manager = manager;
@@ -16,25 +17,29 @@ public class Phylosophers implements Runnable {
         try {
             for (int i = 0; i < 2; i++) {
                 System.out.println("Philosopher " + (id+1) + " thinking " + (i+1) + " time");
+                if(id+1 == manager.MaxEating(arr)){
+                    manager.waiter.release();
+                }
+                else {
+                    manager.waiter.acquire();
+                    manager.forks[leftFork].acquire();
 
-                manager.waiter.acquire();
-                manager.forks[leftFork].acquire();
+                    System.out.println("Philosopher " + (id + 1) + " took left fork");
 
-                System.out.println("Philosopher " + (id + 1) + " took left fork");
+                    manager.forks[rightFork].acquire();
 
-                manager.forks[rightFork].acquire();
+                    System.out.println("Philosopher " + (id + 1) + " took right fork");
+                    System.out.println("Philosopher " + (id + 1) + " eating " + (i + 1) + " time");
+                    arr[id] = i + 1;
+                    manager.waiter.release();
+                    manager.forks[rightFork].release();
 
-                System.out.println("Philosopher " + (id + 1) + " took right fork");
-                System.out.println("Philosopher " + (id + 1) + " eating " + (i + 1) + " time");
+                    System.out.println("Philosopher " + (id + 1) + " put right fork");
 
-                manager.waiter.release();
-                manager.forks[rightFork].release();
+                    manager.forks[leftFork].release();
 
-                System.out.println("Philosopher " + (id + 1) + " put right fork");
-
-                manager.forks[leftFork].release();
-
-                System.out.println("Philosopher " + (id + 1) + " put left fork");
+                    System.out.println("Philosopher " + (id + 1) + " put left fork");
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
